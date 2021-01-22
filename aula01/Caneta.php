60a678a104890f38f99277f4c7b3f081f3374aa4
Jonas Cabral <cabral_jonascabral@hotmail.com>
2021-01-22 18:04:01 -0300

    AULA 1 OBJETOS

---
+++ aula01/Caneta.php
@@ -0,0 +1,24 @@
+<?php
+//CLASSE MODELO DO OBJETO
+    class Caneta{
+        //ATRIBUTOS
+        var $modelo;    
+        var $cor;
+        var $ponta;
+        var $carga;
+        var $tampada;
+
+
+        function rabiscar(){
+
+        }
+        function tampar(){
+
+        }
+        function destampar(){
+
+        }
+        
+}
+
+?>
