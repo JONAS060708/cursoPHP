9c3d7884286952ae09ca820e9b16f21cad2d45d4
Jonas Cabral <cabral_jonascabral@hotmail.com>
2021-01-28 17:40:15 -0300

    aula teórica polimorfismo de sobreposição

---
+++ aula12/teoriaPolimorfismo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+                            POLIMORFISMO de SOBREPOSIÇÃO
+    acontece quando substituímos um método de uma superclasse na sua subclasse , usando a mesma assinatura.
+    Muitas formas-->
+    Permite que o mesmo nome represente vários comportamentos diferentes
+    ASSINATURA DO MÉTODO
+    Quantidade e os tipos dos parâmetros
+
+    Polimorfismo é os mesmos métodos trazer respostas diferentes
+
+
+ */
+
+ ?>
